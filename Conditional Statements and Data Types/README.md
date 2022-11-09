2ec675ce0082f23dc22f00d1f423e8be18067e4a
L-Diasa <67358012+L-Diasa@users.noreply.github.com>
2022-11-09 23:40:32 +0400

    added module 3

---
+++ Conditional Statements and Data Types/README.md
@@ -0,0 +1,45 @@
+# Conditional Statements and Data Types
+## Output of v1
+```
+string is 6
+boolean is true
+number is 2
+string + boolean is 6true
+string + number is 62
+number + boolean is 3
+string * boolean is 6
+string * number is 12
+number * boolean is 2
+string / boolean is 6
+string / number is 3
+number / boolean is 2
+string converted into number is 6 and its type is number
+boolean converted into number is 1 and its type is number
+number converted into string is 2 and its type is string
+boolean converted into string is true and its type is string
+string converted into boolean is true and its type is boolean
+number converted into boolean is true and its type is boolean
+```
+## Output of v2
+```
+string is lika
+boolean is false
+number is 0
+string + boolean is likafalse
+string + number is lika0
+number + boolean is 0
+string * boolean is NaN
+string * number is NaN
+number * boolean is 0
+string / boolean is NaN
+string / number is NaN
+number / boolean is NaN
+string converted into number is NaN and its type is number
+boolean converted into number is 0 and its type is number
+number converted into string is 0 and its type is string
+boolean converted into string is false and its type is string
+string converted into boolean is true and its type is boolean
+number converted into boolean is false and its type is boolean
+```
+
+
---
+++ Conditional Statements and Data Types/v2.js
@@ -0,0 +1,47 @@
+const string = "lika"
+const bool = false
+const number = 0
+console.log(typeof string, "is", string)
+console.log(typeof bool, "is", bool)
+console.log(typeof number, "is", number)
+
+// Perform addition of various types (string + boolean, string + number, number + boolean)
+const sum1 = string + bool
+const sum2 = string + number
+const sum3 = number + bool
+console.log("string + boolean is", sum1)
+console.log("string + number is", sum2)
+console.log("number + boolean is", sum3)
+
+// Perform multiplication of various types (string * boolean, string * number, number * boolean)
+const mul1 = string * bool
+const mul2 = string * number
+const mul3 = number * bool
+console.log("string * boolean is", mul1)
+console.log("string * number is", mul2)
+console.log("number * boolean is", mul3)
+
+// Divide different types (string / boolean, string / number, number / Boolean)
+const div1 = string / bool
+const div2 = string / number
+const div3 = number / bool
+console.log("string / boolean is", div1)
+console.log("string / number is", div2)
+console.log("number / boolean is", div3)
+
+// Perform explicit conversion (number, string, boolean)
+// converting into number
+const strIntoNum = Number(string)
+console.log("string converted into number is", strIntoNum, "and its type is", typeof strIntoNum)
+const boolIntoNum = Number(bool)
+console.log("boolean converted into number is", boolIntoNum, "and its type is", typeof boolIntoNum)
+// converting into string
+const numIntoString = number.toString()
+console.log("number converted into string is", numIntoString, "and its type is", typeof numIntoString)
+const boolIntoString = bool.toString()
+console.log("boolean converted into string is", boolIntoString, "and its type is", typeof boolIntoString)
+// converting into boolean
+const strIntoBool = Boolean(string)
+console.log("string converted into boolean is", strIntoBool, "and its type is", typeof strIntoBool)
+const numIntoBool = Boolean(number)
+console.log("number converted into boolean is", numIntoBool, "and its type is", typeof numIntoBool)
