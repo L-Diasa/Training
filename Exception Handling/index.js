1434c30d7b5cb924e29613dbd9d29fd6e6983239
L-Diasa <67358012+L-Diasa@users.noreply.github.com>
2022-12-10 20:43:37 +0400

    added another module

---
+++ Exception Handling/index.js
@@ -0,0 +1,20 @@
+// In a try catch construction, 
+// wrap the code: console.log (a), let a = 3. 
+// And display an error - ‘let must be declared’ before use.
+try {
+    console.log (a)
+    let a = 3
+} catch(err) {
+    console.log('let must be declared before use.')
+}
+
+// When running 1/0, the error 'cannot be divided by zero'
+try {
+    const num1 = 0
+    const num2 = 1
+    let result = 1/0
+    if(result == 'Infinity')
+        throw new SyntaxError("cannot be divided by zero")
+} catch (err) {
+    console.log(err.message)
+}
