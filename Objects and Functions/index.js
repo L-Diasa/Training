32c8e39e0581303f13f0bfd3a2a45efd282731ad
L-Diasa <67358012+L-Diasa@users.noreply.github.com>
2023-03-05 00:13:29 +0400

    Update index.js

--- Objects and Functions/index.js
+++ Objects and Functions/index.js
@@ -2,15 +2,15 @@
 const car = {
     color: 'black'
 } 
-console.log("The color of the car is", car.color)
+console.log('The color of the car is', car.color)
 
 // Change the color property of the car object to 'green'
 car['color'] = 'green'
-console.log("The color of the car is", car.color)
+console.log('The color of the car is', car.color)
 
 // Add the power property to the car object, 
 // which is a function and displays the engine power to the console
-car.power = () => console.log("The engine power of the car is 300 horsepower")
+car.power = () => console.log('The engine power of the car is 300 horsepower')
 car.power()
 
 
@@ -26,13 +26,13 @@ function total() {
     return warehouse.pears + warehouse.apples
 }
 
-console.log("The number of accepted pears and apples is", total())
+console.log('The number of accepted pears and apples is', total())
 
 // Your name is saved in the payment terminal, 
 // write a function to define the name in the terminal 
 // (if you entered your name, then hello + name, if not, then there is no such name)
 function terminal(name) {
-    name  === 'Lika' ? console.log('hello ' + name) : console.log("there is no such name")
+    name  === 'Lika' ? console.log('hello ' + name) : console.log('there is no such name')
 }
 
 terminal('Lika')
@@ -41,31 +41,28 @@ terminal()
 
 // Write a function for calculating the type of argument and type output to the console
 function type(arg) {
-    console.log("The type of the argument is", typeof arg)
+    console.log('The type of the argument is', typeof arg)
 }
 
 type(4.7)
-type("me")
+type('me')
 type(false)
 
 // Write a function that determines whether a number is prime or not
 function isPrime(num) {
     let isPrime = true
-    if(num == 1) 
-        isPrime = false
-    else if (num !== 2) {
-        let root = num ** 0.5
-        for (let i = 2; i < root; i++) {
+    if(num < 2) isPrime = false
+    else {
+        for (let i = 2; i < num; i++) {
             if(num % i === 0) {
                 isPrime = false
                 break
             }
         }
     }
-    console.log(`The number is ${isPrime ? '' : 'not'} prime`)
+    console.log(`The ${num} is ${isPrime ? '' : 'not'} prime`)
 }
-
-isPrime(1)
-isPrime(2)
-isPrime(8)
-isPrime(11)
+ 
+for(i = -5; i < 11; i++) {
+    isPrime(i)
+}
